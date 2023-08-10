c3127c7d9f93b09841fc21a40d1d481c8fd0b219
Gougogo <1572158030@qq.com>
2023-08-10 20:40:52 +0800

    commit

---
+++ src/main/java/com/goumi/tankgame/Bomb.java
@@ -0,0 +1,26 @@
+package com.goumi.tankgame;
+
+/**
+ * @version 1.0
+ * @auther GouMi
+ */
+public class Bomb {
+    int x, y; //炸弹的坐标
+    int life = 60; //炸弹的生命周期
+    boolean isLive = true; //是否还存活
+
+    public Bomb(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //减少生命值
+    public void lifeDown() { //配合出现图片的爆炸效果
+        if(life > 0) {
+            life--;
+        } else {
+            isLive = false;
+        }
+    }
+
+}
